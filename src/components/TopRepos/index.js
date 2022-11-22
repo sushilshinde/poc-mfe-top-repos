fde5baf2eb6982aeee413872a312e6b9fd1d56d4
Aswathi Suseelan <aswathi.suseelan@AD.MSYSTECHNOLOGIES.COM>
2022-11-22 18:21:48 +0530

    added folders & UI Changes

--- src/components/TopRepos/index.js
+++ src/components/TopRepos/index.js
@@ -1,15 +1,26 @@
+import React, { useEffect, useState } from 'react'
 import "./index.css";
 
 function TopReposs() {
+  const [searchTerm, setSearchTerm] = useState('')
+  useEffect(() => {
+    window.addEventListener('getSearchTerm', (event) => {
+        setSearchTerm(event.detail)
+      });
+    return ()=> {
+      window.removeEventListener('getSearchTerm');
+    };
+  },[])
   return (
     <div className="card p-5 mt-4">
-      <h3 className="mb-4">List of Repositories</h3>
+      <span>{searchTerm}</span>
+      <h3 className="card-header">List of Repositories</h3>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item">React Js</li>
-        <li className="list-group-item">Unix Script Collections</li>
-        <li className="list-group-item">Awesome Node</li>
-        <li className="list-group-item">Awesome CSS</li>
-        <li className="list-group-item">Awesome console</li>
+        <li className="list-group-item"><i class="fa fa-caret-right"></i>React Js</li>
+        <li className="list-group-item"><i class="fa fa-caret-right"></i>Unix Script Collections</li>
+        <li className="list-group-item"><i class="fa fa-caret-right"></i>Awesome Node</li>
+        <li className="list-group-item"><i class="fa fa-caret-right"></i>Awesome CSS</li>
+        <li className="list-group-item"><i class="fa fa-caret-right"></i>Awesome console</li>
       </ul>
     </div>
   );
