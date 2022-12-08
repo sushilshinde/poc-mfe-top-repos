783dc8cb350696d43d2e9b75080409a8ff6997c7
Aswathi Suseelan <aswathi.suseelan@AD.MSYSTECHNOLOGIES.COM>
2022-12-08 20:16:39 +0530

    added ui changes

--- src/components/TopRepos/index.js
+++ src/components/TopRepos/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getRepos } from "../../services/githubService";
+import Loader from "../Loader";
 import Modal from "../Modal";
 import "./index.css";
 
@@ -12,7 +13,12 @@ import "./index.css";
 function TopRepos() {
     const [searchTerm, setSearchTerm] = useState("");
     const [repos, setRepos] = useState([]);
-    const [showModal, setShowModal] = useState({ show: false, text: '', searchTerm })
+    const [loading, setLoading] = useState(false);
+    const [showModal, setShowModal] = useState({
+        show: false,
+        text: "",
+        searchTerm,
+    });
     useEffect(() => {
         window.addEventListener("getSearchTerm", (event) => {
             setSearchTerm(event.detail);
@@ -24,33 +30,53 @@ function TopRepos() {
 
     useEffect(() => {
         if (searchTerm.trim()) {
-            getRepos(searchTerm).then(data => {
-                if(data.status === 404) {
-                    setRepos([]) 
-                    setShowModal({ show: true, text: `No Repositories found for `, searchTerm })
-                }else if(data.length === 0) {
-                    setRepos([]) 
-                    setShowModal({ show: true, text: `No Repositories found for `, searchTerm })
-                }else {
-                    setRepos(data) 
-                    setShowModal({ show: false, text: '', searchTerm: '' })
+            setRepos([])
+            setLoading(true);
+            getRepos(searchTerm).then((data) => {
+                setLoading(false);
+                if (data.status === 404) {
+                    setRepos([]);
+                    setShowModal({
+                        show: true,
+                        text: `No Repositories found for `,
+                        searchTerm,
+                    });
+                } else if (data.length === 0) {
+                    setRepos([]);
+                    setShowModal({
+                        show: true,
+                        text: `No Repositories found for `,
+                        searchTerm,
+                    });
+                } else {
+                    setRepos(data);
+                    setShowModal({ show: false, text: "", searchTerm: "" });
                 }
-            })
-        }else {
-			setRepos([])
-		}
+            });
+        } else {
+            setRepos([]);
+            setLoading(false);
+        }
     }, [searchTerm]);
 
     return (
         <div className="card p-5 mt-4">
-            <Modal show={repos.length === 0 && showModal.show} setShowModal={setShowModal} text={showModal.text} searchTerm={searchTerm}/>
-            <h3 className="card-header">List of Repositories</h3>
+            <Modal
+                show={repos.length === 0 && showModal.show}
+                setShowModal={setShowModal}
+                text={showModal.text}
+                searchTerm={searchTerm}
+            />
+            <h3 className="card-header">Top Repositories</h3>
             <ul className="list-group list-group-flush">
+                {loading && <Loader />}
                 {repos.length > 0 &&
                     repos.map(({ id, name, html_url }) => (
                         <li className="list-group-item" key={id}>
                             <i class="fa fa-caret-right"></i>
-                            <a href={html_url} target="_blank" rel="noreferrer">{name}</a>
+                            <a href={html_url} target="_blank" rel="noreferrer">
+                                {name}
+                            </a>
                         </li>
                     ))}
             </ul>
